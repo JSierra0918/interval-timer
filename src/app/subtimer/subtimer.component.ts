79788bd1c8ec8be8693323adca2b3af34e74ad17
Jorge Sierra <3d.jorgesierra@gmail.com>
2020-12-05 14:35:54 -0500

    added logic to stop subtimer from being higher than maintimer

--- src/app/subtimer/subtimer.component.ts
+++ src/app/subtimer/subtimer.component.ts
@@ -2,21 +2,16 @@ import { Component, Input, OnInit } from '@angular/core';
 import { TimerModel } from '../models/main-timer';
 
 @Component({
-  selector: 'app-subtimer',
-  templateUrl: './subtimer.component.html',
-  styleUrls: ['./subtimer.component.scss'],
+	selector: 'app-subtimer',
+	templateUrl: './subtimer.component.html',
+	styleUrls: ['./subtimer.component.scss'],
 })
 export class SubtimerComponent implements OnInit {
+	testArray: Array<number> = new Array(10);
+	@Input() subtime: TimerModel;
 
-  testArray: Array<number> = new Array(10);
-  @Input() subtime: TimerModel;
+	constructor() {}
 
-  constructor() { }
-
-  // TODO: create subtimer logic where it cannot be set higher than main timer. It cannot pass main timer.  If it's higher than main timer, then match it.
-  ngOnInit() {
-    console.log(this.subtime);
-
-  }
-
+	// TODO: create subtimer logic where it cannot be set higher than main timer. It cannot pass main timer.  If it's higher than main timer, then match it.
+	ngOnInit() {}
 }
--- src/app/timer-container/timer-container.component.ts
+++ src/app/timer-container/timer-container.component.ts
@@ -1,110 +1,110 @@
-import { Component, OnInit } from "@angular/core";
-import { timer } from "rxjs";
-import { TimerModel, CountDownTimer } from "../models/main-timer";
+import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
+import { TimerModel, CountDownTimer } from '../models/main-timer';
 import { SubtimerComponent } from '../subtimer/subtimer.component';
 
 @Component({
-  selector: "app-timer-container",
-  templateUrl: "./timer-container.component.html",
-  styleUrls: ["./timer-container.component.scss"],
+	selector: 'app-timer-container',
+	templateUrl: './timer-container.component.html',
+	styleUrls: ['./timer-container.component.scss'],
 })
 export class TimerContainerComponent implements OnInit {
-  time: TimerModel = { main: {}, subtimer: [] };
-  timerInterval: any;
+	time: TimerModel = { main: {}, subtimer: [] };
+	timerInterval: any;
 
-  constructor() {}
+	constructor() {}
 
-  ngOnInit() {
-    this.time.main = {
-      h: 0,
-      m: 1,
-      s: 0,
-    };
+	ngOnInit() {
+		this.time.main = {
+			h: 0,
+			m: 0,
+			s: 20,
+		};
 
-    this.time.subtimer = [
-      {
-        h: 0,
-        m: 0,
-        s: 15,
-      }, 
-      {
-        h: 0,
-        m: 0,
-        s: 5,
-      }
-    ]
-  }
+		this.time.subtimer = [
+			{
+				h: 0,
+				m: 0,
+				s: 10,
+			},
+			{
+				h: 0,
+				m: 0,
+				s: 5,
+			},
+		];
+	}
 
-  startTheTime() {
-    if (this.time.main.isRunning) {
-      return;
-    }
+	startTheTime() {
+		if (this.time.main.isRunning) {
+			return;
+		}
 
-    this.time.main.isRunning = true;
-    this.timerInterval = setInterval(() => {
-      const mainTimer = this.timerCountDown(this.time.main);
-      //Assign to main timer; has to be a better way than this.
-      this.time.main.h = mainTimer.h;
-      this.time.main.m = mainTimer.m;
-      this.time.main.s = mainTimer.s;
+		this.time.main.isRunning = true;
+		this.timerInterval = setInterval(() => {
+			const mainTimer = this.timerCountDown(this.time.main);
 
-      //figure out best way to do this.  May need a function for it
-      this.time.subtimer.forEach(sTime => {
-        const subTimer = this.timerCountDown(sTime);
-        sTime.h = subTimer.h;
-        sTime.m = subTimer.m;
-        sTime.s = subTimer.s;
-        console.log("subTime")
-        console.log(sTime.h, sTime.m, sTime.s);
-      });
-    }, 1000);
+			//figure out best way to do this.  May need a function for it
+			this.time.subtimer.forEach((sTime) => {
+				let subTimer = this.timerCountDown(sTime);
 
-  }
+				// subtimers cannot go longer than main timer; makes subtimers equal to main if they're higher
+				if (mainTimer.h < subTimer.h) subTimer.h = mainTimer.h;
+				if (mainTimer.m < subTimer.m) subTimer.m = mainTimer.m;
+        if (mainTimer.s < subTimer.s) subTimer.s = mainTimer.s;
+        
+        subTimer = {...this.addPadding(subTimer)};
+				sTime.h = subTimer.h;
+				sTime.m = subTimer.m;
+				sTime.s = subTimer.s;
+			});
 
-  pauseTheTimer() {
-    this.time.main.isRunning = false;
-    clearInterval(this.timerInterval);
-  }
+			this.time.main = { ...this.time.main, ...this.addPadding(mainTimer) };
+		}, 1000);
+	}
 
-  timerCountDown({ h, m, s }: CountDownTimer): CountDownTimer {
-    // get Total seconds
-    let totalSeconds = this.convertToTotalSeconds(
-      Number(h),
-      Number(m),
-      Number(s)
-    );
+	pauseTheTimer() {
+		this.time.main.isRunning = false;
+		clearInterval(this.timerInterval);
+	}
 
-    if (totalSeconds > 0) {
-      totalSeconds--;
+	timerCountDown({ h, m, s }: CountDownTimer): CountDownTimer {
+		// get Total seconds
+		let totalSeconds = this.convertToTotalSeconds(Number(h), Number(m), Number(s));
 
-      //convert back to hours, minutes, seconds
-      h = Math.floor(totalSeconds / 3600);
-      totalSeconds %= 3600;
-      m = Math.floor(totalSeconds / 60);
-      s = totalSeconds % 60;
+		if (totalSeconds > 0) {
+			totalSeconds--;
 
-      // add padding
-      h = this.addPadding(h);
-      m = this.addPadding(m);
-      s = this.addPadding(s);
-    } else {
-      // main timer is done.
-      h = this.addPadding(0);
-      m = this.addPadding(0);
-      s = this.addPadding(0);
-    }
+			//convert back to hours, minutes, seconds
+			h = Math.floor(totalSeconds / 3600);
+			totalSeconds %= 3600;
+			m = Math.floor(totalSeconds / 60);
+			s = totalSeconds % 60;
+		} else {
+			// main timer is done.
+			h = 0;
+			m = 0;
+			s = 0;
+		}
 
-    return { h, m, s };
-  }
+		return { h, m, s };
+	}
 
-  convertToTotalSeconds(h: number, m: number, s: number): number {
-    const hoursAsSeconds = h * 3600;
-    const minutesAsSeconds = m * 60;
+	convertToTotalSeconds(h: number, m: number, s: number): number {
+		const hoursAsSeconds = h * 3600;
+		const minutesAsSeconds = m * 60;
 
-    return hoursAsSeconds + minutesAsSeconds + s;
-  }
+		return hoursAsSeconds + minutesAsSeconds + s;
+	}
 
-  addPadding(t) {
-    return t.toString().padStart(2, "0");
-  }
+	addPadding(t: CountDownTimer): CountDownTimer {
+		let paddedT: CountDownTimer = { ...t };
+		for (const key in t) {
+			if (paddedT[key] !== null) {
+				paddedT[key] = t[key].toString().padStart(2, '0');
+			}
+		}
+
+		return paddedT;
+	}
 }
