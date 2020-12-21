0eea940cdaea62680a192d0da5d2e27a6ad2c963
Jorge <3d.jorgesierra@gmail.com>
2020-12-21 09:06:05 -0500

    revised logic for input validation

--- src/app/profile-form/profile-form.component.ts
+++ src/app/profile-form/profile-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, Validators, AbstractControl, FormGroup } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
+import { filter, tap } from 'rxjs/operators';
 import content from '../../content/content.json';
 import { StorageItem } from '../models/storage-item';
 import { StorageService } from '../services/storage.service';
@@ -16,16 +17,24 @@ export class ProfileFormComponent implements OnInit {
 	profileForm = this.fb.group({
 		profileName: ['', [Validators.required]],
 		mainTimeGroup: this.fb.group({
-			h: [null, [Validators.maxLength(2)]],
-			m: [null, [Validators.maxLength(2)]],
-			s: [null, [Validators.maxLength(2)]],
+			h: [null, [Validators.min(0)]],
+			m: [null, [Validators.min(0)]],
+			s: [null, [Validators.min(0)]],
 		}),
 		subTimeGroup: this.fb.array([]),
 	});
 
 	constructor(private modalController: ModalController, private fb: FormBuilder, private storageService: StorageService) { }
 
-	ngOnInit() { }
+	ngOnInit() { this.onChanges() }
+
+	get mainTimeForm() {
+		return this.profileForm.get('mainTimeGroup') as FormArray;
+	}
+
+	get subtimeForm() {
+		return this.profileForm.get('subTimeGroup') as FormArray;
+	}
 
 	async dismissModal() {
 		this.modalController.dismiss({
@@ -33,15 +42,11 @@ export class ProfileFormComponent implements OnInit {
 		});
 	}
 
-	get subtimeForm() {
-		return this.profileForm.get('subTimeGroup') as FormArray;
-	}
-
-	addNewSubtimer() {
+	addNewSubtimer(): void {
 		const subtime = this.fb.group({
-			h: [null],
-			m: [null],
-			s: [null],
+			h: [null, [Validators.min(0)]],
+			m: [null, [Validators.min(0)]],
+			s: [null, [Validators.min(0)]],
 		});
 
 		this.subtimeForm.push(subtime);
@@ -67,17 +72,27 @@ export class ProfileFormComponent implements OnInit {
 		this.subtimeForm.removeAt(i);
 	}
 
-	minimumNumbers(e) {
-		const length = e.target.value.length;
-		if (e.code === "KeyE") { return false }
-		if (length === 2) { e.target.value = e.target.value.slice(0, 1) }
+	private onChanges(): void {
+		const mainTimeGroupRestraints = this.mainTimeForm.valueChanges.pipe(tap(val => {
+			if (val.h > 24 || val.h < 0) { this.mainTimeForm.patchValue({ ...val, h: 24 }) }
+			if (val.m > 60) { this.mainTimeForm.patchValue({ ...val, m: 60 }) }
+			if (val.s > 60) { this.mainTimeForm.patchValue({ ...val, s: 60 }) }
+		}));
+
+		const subtimeGroupRestraints =this.subtimeForm.valueChanges.pipe(tap(val => {
+			if (val.h > 24) { this.subtimeForm.patchValue({ ...val, h: 24 }) }
+			if (val.m > 60) { this.subtimeForm.patchValue({ ...val, m: 60 }) }
+			if (val.s > 60) { this.subtimeForm.patchValue({ ...val, s: 60 }) }
+		}));
+		mainTimeGroupRestraints.subscribe();
+		subtimeGroupRestraints.subscribe()
+
+		// subscribe to formArray
 	}
 
-	maxInput(e, maxNum: number) {
-		const val = e.target.value;
-		console.log(e);
-		if (val >= maxNum) {console.log(val); e.target.value = maxNum }
+	preventEValue(e): void {
+		if (e.which != 8 && e.which != 0 && e.which < 48 || e.which > 57) {
+			e.preventDefault();
+		}
 	}
-
-
 }
