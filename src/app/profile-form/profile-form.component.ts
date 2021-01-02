b48ab2e88a29d7b6f11e519fa415e4eba490574f
Jorge Sierra <3d.jorgesierra@gmail.com>
2021-01-02 17:24:26 -0500

    Finally figured out how to limit user input through Form Arrays

--- src/app/profile-form/profile-form.component.ts
+++ src/app/profile-form/profile-form.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, Validators, AbstractControl, FormGroup, Form } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
-import { filter, tap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil, tap } from 'rxjs/operators';
 import content from '../../content/content.json';
 import { StorageItem } from '../models/storage-item';
 import { StorageService } from '../services/storage.service';
@@ -11,8 +12,9 @@ import { StorageService } from '../services/storage.service';
 	templateUrl: './profile-form.component.html',
 	styleUrls: ['./profile-form.component.scss'],
 })
-export class ProfileFormComponent implements OnInit {
+export class ProfileFormComponent implements OnInit, OnDestroy {
 	content = content;
+	unsubscribe$: Subject<any> = new Subject();
 
 	profileForm = this.fb.group({
 		profileName: ['', [Validators.required]],
@@ -24,9 +26,20 @@ export class ProfileFormComponent implements OnInit {
 		subtimeArrayGroup: this.fb.array([]),
 	});
 
-	constructor(private modalController: ModalController, private fb: FormBuilder, private storageService: StorageService) { }
+	constructor(private modalController: ModalController, private fb: FormBuilder, private storageService: StorageService) {}
 
-	ngOnInit() { this.onChanges() }
+	ngOnInit() {}
+
+	ngOnDestroy() {
+		this.unsubscribe$.next();
+		this.unsubscribe$.complete();
+	}
+
+	ionViewDidEnter() {
+		this.watchMainGroupChanges();
+	}
+
+	//getters
 
 	get mainTimeForm() {
 		return this.profileForm.get('mainTimeGroup') as FormGroup;
@@ -34,6 +47,10 @@ export class ProfileFormComponent implements OnInit {
 
 	get subtimeArray() {
 		return this.profileForm.get('subtimeArrayGroup') as FormArray;
+	}
+
+	get subtimeForm() {
+		return this.subtimeArray.get('subtimeGroup') as FormGroup;
 	}
 
 	async dismissModal() {
@@ -49,6 +66,7 @@ export class ProfileFormComponent implements OnInit {
 			s: [null, [Validators.min(0)]],
 		});
 
+		this.watchSubtimeGroupChanges(subtimeGroup);
 		this.subtimeArray.push(subtimeGroup);
 	}
 
@@ -64,40 +82,50 @@ export class ProfileFormComponent implements OnInit {
 	}
 
 	maxOfTwentyFourHours(c: AbstractControl): { [key: string]: boolean } | null {
-
 		return null;
 	}
 
 	deleteSubtimeForm = (i) => {
 		this.subtimeArray.removeAt(i);
-	}
+	};
 
-	private onChanges(): void {
-		const mainTimeGroupRestraints =	this.mainTimeForm.valueChanges.pipe(tap(val => {
-			if (val.h > 24 || val.h < 0) { this.mainTimeForm.patchValue({ ...val, h: 24 }) }
-			if (val.m > 60) { this.mainTimeForm.patchValue({ ...val, m: 60 }) }
-			if (val.s > 60) { this.mainTimeForm.patchValue({ ...val, s: 60 }) }
-		}));
-
-		this.subtimeArray.valueChanges.pipe(tap((form => {
-			for (const val of form) {
-				if (val.h > 24) { val.h = 24 }
-				if (val.m > 60) { val.m = 60 }
-				if (val.s > 60) { val.s = 60 }
-
-			}
-
-			console.log(form);
-		})));
+	//watchers
+	private watchMainGroupChanges(): void {
+		const mainTimeGroupRestraints = this.mainTimeForm.valueChanges.pipe(
+			takeUntil(this.unsubscribe$),
+			tap((val) => {
+				console.log(this.profileForm);
+				if (val.h > 24 || val.h < 0) {
+					this.mainTimeForm.patchValue({ ...val, h: 24 });
+				}
+				if (val.m > 60) {
+					this.mainTimeForm.patchValue({ ...val, m: 60 });
+				}
+				if (val.s > 60) {
+					this.mainTimeForm.patchValue({ ...val, s: 60 });
+				}
+			})
+		);
 
 		mainTimeGroupRestraints.subscribe();
-		// subtimeGroupRestraints.subscribe()
+	}
 
-		// subscribe to formArray
+	private watchSubtimeGroupChanges(subtimeGroup): void {
+		subtimeGroup.valueChanges.pipe(takeUntil(this.unsubscribe$), debounceTime(1000)).subscribe((val) => {
+			if (val.h > 24 || val.h < 0) {
+				subtimeGroup.patchValue({ ...val, h: 24 });
+			}
+			if (val.m > 60) {
+				subtimeGroup.patchValue({ ...val, m: 60 });
+			}
+			if (val.s > 60) {
+				subtimeGroup.patchValue({ ...val, s: 60 });
+			}
+		});
 	}
 
 	preventEValue(e): void {
-		if (e.which != 8 && e.which != 0 && e.which < 48 || e.which > 57) {
+		if ((e.which != 8 && e.which != 0 && e.which < 48) || e.which > 57) {
 			e.preventDefault();
 		}
 	}
